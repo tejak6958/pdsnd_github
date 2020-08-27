7101607d633d2ff49749ce3a564ca223b75b6221
Your Name <you@example.com>
2020-08-27 17:59:30 +0530

    added comment lines to the #udacity152

--- bikeshare.py
+++ bikeshare.py
@@ -14,7 +14,7 @@ def get_filters():
     Returns:
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+        (str) day - name of the days of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
